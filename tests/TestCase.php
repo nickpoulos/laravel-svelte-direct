9c9b6fc15b861de1407b9ccb14f723ad0c35f907
Nick Poulos <nickpoulos.ny@gmail.com>
2021-05-20 22:49:56 -0400

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,36 +1,26 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace Nickpoulos\SvelteDirect\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use VendorName\Skeleton\SkeletonServiceProvider;
+use Nickpoulos\SvelteDirect\SvelteDirectServiceProvider;
 
 class TestCase extends Orchestra
 {
     public function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'VendorName\\Skeleton\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
     protected function getPackageProviders($app)
     {
         return [
-            SkeletonServiceProvider::class,
+            SvelteDirectServiceProvider::class,
         ];
     }
 
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
-        /*
-        include_once __DIR__.'/../database/migrations/create_skeleton_table.php.stub';
-        (new \CreatePackageTable())->up();
-        */
     }
 }
