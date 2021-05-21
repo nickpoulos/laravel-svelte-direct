f8fdfc643cb8045744aa063742e02fbb4dd8fd69
Nick Poulos <nickpoulos.ny@gmail.com>
2021-05-21 16:05:45 -0400

    Just combine everything into the single service provider
    Keep things simple, don't have to pass $app around

--- src/SvelteDirectServiceProvider.php
+++ src/SvelteDirectServiceProvider.php
@@ -2,15 +2,71 @@
 
 namespace Nickpoulos\SvelteDirect;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class SvelteDirectServiceProvider extends ServiceProvider
 {
+    public ?array $manifest;
+    public array $tagsBeingUsed;
+
     public function boot(): void
     {
-        $svelteDirect = new SvelteDirect();
-        $svelteDirect->loadManifestFile();
-        $svelteDirect->loadBladePreCompiler();
-        $svelteDirect->loadBladeDirective();
+        $this->loadManifestFile();
+        $this->loadBladePreCompiler();
+        $this->loadBladeDirective();
+    }
+
+    public function defaultManifestPath() : string
+    {
+        return App::bootstrapPath('cache/svelte-direct-components.php');
+    }
+
+    public function loadManifestFile(?string $manifestFilePath = null) : void
+    {
+        $files = new Filesystem();
+        $manifestPath = $manifestFilePath ?? $this->defaultManifestPath();
+        $this->manifest = file_exists($manifestPath) ? $files->getRequire($manifestPath):null;
+    }
+
+    public function loadBladePreCompiler() : void
+    {
+        if (! $this->manifest) {
+            return;
+        }
+
+        $this->app['blade.compiler']->precompiler([$this, 'findTagsInBladeTemplate']);
+    }
+
+    public function loadBladeDirective() : void
+    {
+        if (! $this->manifest) {
+            return;
+        }
+
+        Blade::directive('sveltedirect', [$this, 'generateDirectiveHtml']);
+    }
+
+    /** @internal */
+    public function findTagsInBladeTemplate(string $view) : string
+    {
+        $tagPattern = implode('|', array_keys($this->manifest));
+        $pattern = "/(?<=<)\s*{$tagPattern}/";
+        preg_match_all($pattern, $view, $matches);
+        $this->tagsBeingUsed = array_merge(array_unique($matches[0]), $this->tagsBeingUsed);
+
+        return $view;
+    }
+
+    /** @internal */
+    public function generateDirectiveHtml(string $expression) : string
+    {
+        $tagsToLoad = array_intersect(array_keys($this->manifest), $this->tagsBeingUsed);
+
+        return array_reduce($tagsToLoad, function ($previous, $current) {
+            return $previous . '<script src="{{ mix("' . $this->manifest[$current] . '") }}"></script>' . PHP_EOL;
+        }, '');
     }
 }
