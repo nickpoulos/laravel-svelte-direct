6586d7da162ce48001fc7e29f3562786e58c94f9
Nick Poulos <nickpoulos.ny@gmail.com>
2021-05-29 02:45:28 -0400

    First pass at SvelteDirectManifestBuilder class

---
+++ src/Support/SvelteDirectManifestBuilder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Nickpoulos\SvelteDirect\Support;
+
+use PHPHtmlParser\Dom;
+use Illuminate\Filesystem\Filesystem;
+
+class SvelteDirectManifestBuilder
+{
+    public function build()
+    {
+        $serverless = false;
+
+        $defaultManifestPath = $serverless
+            ? '/tmp/storage/bootstrap/cache/svelte-direct-components.php'
+            : app()->bootstrapPath('cache/svelte-direct-components.php');
+
+        $files = new Filesystem();
+
+        $svelteFiles = array_filter(
+            $files->allFiles(resource_path('js')),
+            static fn ($file) => $file->getExtension() === 'svelte'
+        );
+
+        $tagNames = collect($svelteFiles)
+            ->keyBy(fn($file) => $file->getRealPath())
+            ->map(function(\SplFileInfo $file) use ($files) {
+                return $files->get($file->getRealPath());
+            })
+            ->filter(function(string $code) {
+                $pattern = '/<\s*svelte:options\s.+>/';
+                return preg_match($pattern, $code, $matches);
+            })
+            ->map(function(string $code) use ($files) {
+                $pattern = '/<\s*svelte:options\s.+>/';
+                preg_match_all($pattern, $code, $matches);
+                return (new Dom)->loadStr($matches[0][0])->find('svelte:options')[0]?->tag;
+            })
+            ->flip();
+    }
+
+}
